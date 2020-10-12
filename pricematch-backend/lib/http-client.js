b9b4d9676b8369e4b2b250d70e36fa4f48ba2b1a
simonye <simonysy2012@gmail.com>
2020-10-12 17:43:01 -0400

    Update http-client.js

--- pricematch-backend/lib/http-client.js
+++ pricematch-backend/lib/http-client.js
@@ -1,11 +1,3 @@
-/******************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2016. All Rights Reserved.
- *
- * Note to U.S. Government Users Restricted Rights:
- * Use, duplication or disclosure restricted by GSA ADP Schedule
- * Contract with IBM Corp.
- ******************************************************************************/
 'use strict';
 
 var https = require('https');
